b23de8f2eef422ad96b07258d9b38e60faff5a83
Med771 <mm7737333@gmail.com>
2025-09-22 13:03:56 +0300

    Init commit

---
+++ src/test/java/ru/sin/narspiknwja/NarspiKnwJaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sin.narspiknwja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarspiKnwJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
