7211302a55af0a001f4326cf6b54d51912229026
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-09-26 14:17:29 +0300

    Add tools

---
+++ src/main/java/ru/sin/narspiknwja/tools/RabbitTools.java
@@ -0,0 +1,41 @@
+package ru.sin.narspiknwja.tools;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import ru.sin.narspiknwja.body.HistoryReq;
+import ru.sin.narspiknwja.body.HistoryRes;
+import ru.sin.narspiknwja.body.QueryReq;
+import ru.sin.narspiknwja.body.QueryRes;
+import ru.sin.narspiknwja.config.RabbitConfig;
+import ru.sin.narspiknwja.model.History;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitTools {
+    private final Logger logger = LoggerFactory.getLogger(RabbitTools.class);
+
+    @RabbitListener(queues = RabbitConfig.queryReqQueue, concurrency = "3-5")
+    public QueryRes handleQuery(QueryReq req) {
+        logger.info("[UUID: {}] Handle query", req.uuid());
+
+        List<String> docs = List.of("OK", "OK");
+
+        return new QueryRes(req.uuid(), docs);
+    }
+
+    @RabbitListener(queues = RabbitConfig.historyReqQueue, concurrency = "1-2")
+    public HistoryRes handleHistory(HistoryReq req) {
+        logger.info("[UUID: {}] Handle history", req.uuid());
+
+        List<History> history = List.of(
+                new History(LocalDateTime.now(), "query", LocalDateTime.now(), "answer"));
+
+        return new HistoryRes(req.uuid(), history);
+    }
+}
